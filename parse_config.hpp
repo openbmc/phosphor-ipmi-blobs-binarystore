eb197f0197aea5c5997789fc23611c3ea2279b36
Kun Yi <kunyi731@gmail.com>
2019-03-05 16:06:19 -0800

    Implement config parsing
    Add a configuration json parsing function to read the configuration file
    from /usr/share/binaryblob/config.json for which system file and location
    to serialize/deserialize the binarystore blobs. Add a simple unit test for
    parsing configs as well.
    
    Signed-off-by: Kun Yi <kunyi731@gmail.com>
    Change-Id: Ie86d622e83991365bc202d659f5860ff01190311

---
+++ parse_config.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <cstdint>
+#include <nlohmann/json.hpp>
+#include <string>
+
+using std::uint32_t;
+using json = nlohmann::json;
+
+namespace conf
+{
+
+struct BinaryBlobConfig
+{
+    std::string blobBaseId;    // Required
+    std::string sysFilePath;   // Required
+    uint32_t offsetBytes = 0;  // Optional
+    uint32_t maxSizeBytes = 0; // Optional
+};
+
+/**
+ * @brief Parse parameters from a config json
+ * @param j: input json object
+ * @param config: output BinaryBlobConfig
+ * @throws: exception if config doens't have required fields
+ */
+static inline void parseFromConfigFile(const json& j, BinaryBlobConfig& config)
+{
+    j.at("blobBaseId").get_to(config.blobBaseId);
+    j.at("sysFilePath").get_to(config.sysFilePath);
+    if (j.find("offsetBytes") != j.end())
+    {
+        j.at("offsetBytes").get_to(config.offsetBytes);
+    }
+    if (j.find("maxSizeBytes") != j.end())
+    {
+        j.at("maxSizeBytes").get_to(config.maxSizeBytes);
+    }
+}
+
+} // namespace conf
